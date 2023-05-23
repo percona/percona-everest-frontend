615797a45eecfa242e01d5ef3acea95838bd3327
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-05-23 19:29:54 +0400

    EVEREST-56-styles-of-buttons: fix paddings, fix font size, fix disable colors

--- design/themes/everest/EverestTheme.tsx
+++ design/themes/everest/EverestTheme.tsx
@@ -22,7 +22,6 @@ const themeOptions = (mode: PaletteMode): ThemeOptions => ({
       textTransform: 'none',
       lineHeight: '1',
       letterSpacing: '0.025em',
-      fontWeight: 500,
     },
   },
   palette: {
@@ -36,11 +35,15 @@ const themeOptions = (mode: PaletteMode): ThemeOptions => ({
             selectedOpacity: 0.08,
             focus: 'rgba(119, 79, 170, 0.12)',
             focusOpacity: 0.12,
+            disabled: 'rgba(44, 50, 62, 0.12)',
           },
           primary: {
             main: '#0E5FB5',
             light: '#127AE8',
             dark: '#0B4A8C',
+          },
+          text: {
+            disabled: 'rgba(48, 54, 66, 0.5)',
           },
         }
       : {
@@ -51,18 +54,22 @@ const themeOptions = (mode: PaletteMode): ThemeOptions => ({
             selectedOpacity: 0.12,
             focus: 'rgba(245, 204, 120, 0.15)',
             focusOpacity: 0.15,
+            disabled: 'rgba(240, 241, 244, 0.14)',
           },
           primary: {
             main: '#62AEFF',
             light: '#B6D9FF',
             dark: '#1486FF',
           },
+          text: {
+            disabled: 'rgba(255, 255, 255, 0.4)',
+          },
         }),
   },
   components: {
     MuiButton: {
       styleOverrides: {
-        root: ({ ownerState }) => ({
+        root: ({ ownerState, theme }) => ({
           borderRadius: '128px',
           borderWidth: '2px',
           ...(ownerState.variant === 'contained' && {
@@ -78,20 +85,33 @@ const themeOptions = (mode: PaletteMode): ThemeOptions => ({
           }),
           ...(ownerState.variant === 'outlined' && {
             ...(ownerState.size === 'large' && {
-              padding: '13px 22px',
+              padding: '11px 22px',
             }),
             ...(ownerState.size === 'medium' && {
-              padding: '11px 14px',
+              padding: '9px 14px',
             }),
             ...(ownerState.size === 'small' && {
-              padding: '8px 10px',
+              padding: '6px 10px',
             }),
+          }),
+          ...(ownerState.size === 'large' && {
+            fontSize: '16px',
+          }),
+          ...(ownerState.size === 'medium' && {
+            fontSize: '16px',
+          }),
+          ...(ownerState.size === 'small' && {
+            fontSize: '14px',
           }),
           '&: hover': {
             borderWidth: '2px',
           },
           '&: disabled': {
             borderWidth: '2px',
+            ...(ownerState.variant === 'contained' && {
+              backgroundColor: theme.palette.action.disabled,
+            }),
+            color: theme.palette.text.disabled,
           },
         }),
       },
