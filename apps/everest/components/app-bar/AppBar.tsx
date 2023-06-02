8a0206612918e8493a4663a856bb3f93f1332fa3
Fabio Silva <ffjs1993@gmail.com>
2023-06-02 13:30:17 +0100

    everest-60 remove IconButton

--- apps/everest/components/app-bar/AppBar.tsx
+++ apps/everest/components/app-bar/AppBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, IconButton, AppBar as MuiAppBar, Toolbar } from '@mui/material';
+import { Box, AppBar as MuiAppBar, Toolbar } from '@mui/material';
 import { EverestHorizontalIcon } from '@percona/ui-lib.icons.everest';
 
 export const AppBar = () => {
@@ -7,12 +7,7 @@ export const AppBar = () => {
     <Box>
       <MuiAppBar position='static'>
         <Toolbar>
-          <IconButton
-            edge='start'
-            aria-label="menu"
-          >
-            <EverestHorizontalIcon sx={{ height: '32px', width: 'auto' }} />
-          </IconButton>
+          <EverestHorizontalIcon sx={{ height: '32px', width: 'auto' }} />
         </Toolbar>
       </MuiAppBar>
     </Box>
