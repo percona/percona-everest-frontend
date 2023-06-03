6ea9034f8463fb44ca2ae36eb26d52197a214a4e
Fabio Silva <ffjs1993@gmail.com>
2023-06-04 00:26:32 +0100

    everest-60 use react-router-dom recent feats

--- apps/everest/app.tsx
+++ apps/everest/app.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { ThemeContextProvider } from '@percona/design.theme-context-provider';
 import { everestThemeOptions } from '@percona/design.themes.everest';
 import { AppBar } from './components/app-bar/AppBar';
 import { Drawer } from './components/drawer/Drawer';
 import { DrawerContext } from './components/drawer/Drawer.context';
-import { Box, Toolbar, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Toolbar, useMediaQuery, useTheme } from '@mui/material';
 
 const BarAndDrawer = () => {
   const [open, setOpen] = useState(false);
@@ -30,25 +30,7 @@ export const EverestApp = () => {
         <BarAndDrawer />
         <Box component='main' sx={{ padding: 4 }}>
           <Toolbar />
-          <Routes>
-            <Route path="/" element={
-              <Typography paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                sapien faucibus et molestie ac.
-              </Typography>
-            }>
-            </Route>
-          </Routes>
+          <Outlet />
         </Box>
       </Box>
     </ThemeContextProvider >
--- apps/everest/everest.app-root.tsx
+++ apps/everest/everest.app-root.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { EverestApp } from './app';
+import { Typography } from '@mui/material';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <EverestApp />,
+    children: [{
+      path: 'databases',
+      element: (
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+          sapien faucibus et molestie ac.
+        </Typography>
+      )
+    }]
+  },
+]);
 
 ReactDOM.render((
-  <BrowserRouter>
-    <EverestApp />
-  </BrowserRouter>
+  <RouterProvider router={router} />
 ), document.getElementById('root'));
 
