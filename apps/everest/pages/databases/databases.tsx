417e3c9fb4e1fa8083807fc49313cdea09933df5
Fabio Silva <ffjs1993@gmail.com>
2023-06-06 09:46:46 +0100

    everest-95 add NewDatabasePage

--- apps/everest/pages/databases/databases.tsx
+++ apps/everest/pages/databases/databases.tsx
@@ -6,14 +6,16 @@ import { Link } from 'react-router-dom';
 
 export const DatabasesPage = () => {
   return (
-    <Stack direction='row' alignItems='center'>
-      <Typography variant='h5'>Databases</Typography>
-      <Button size='small'
+    <Stack direction="row" alignItems="center">
+      <Typography variant="h5">Databases</Typography>
+      <Button
+        size="small"
         sx={{ marginLeft: 'auto' }}
         startIcon={<AddIcon />}
         component={Link}
-        to='/databases/new'
-        variant="outlined">
+        to="/databases/new"
+        variant="outlined"
+      >
         Create Database
       </Button>
     </Stack>
