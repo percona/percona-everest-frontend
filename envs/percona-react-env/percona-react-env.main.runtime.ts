c09bf781c1a18da8976b404581a421c0d0b00cb8
Iaroslavna Soloveva <90199600+solovevayaroslavna@users.noreply.github.com>
2023-06-07 17:27:43 +0400

    Everest 59 stepper (#7)
    EVEREST-59-stepper

--- envs/percona-react-env/percona-react-env.main.runtime.ts
+++ envs/percona-react-env/percona-react-env.main.runtime.ts
@@ -19,15 +19,14 @@ export class PerconaReactEnvMain {
   static runtime = MainRuntime;
 
   static async provider([react, envs]: [ReactMain, EnvsMain]) {
-
     //const webpackModifiers: UseWebpackModifiers = {
-      //  previewConfig: [previewConfigTransformer],
-      //  devServerConfig: [devServerConfigTransformer],
+    //  previewConfig: [previewConfigTransformer],
+    //  devServerConfig: [devServerConfigTransformer],
     //};
 
     //const tsModifiers: UseTypescriptModifiers = {
-      //  devConfig: [devConfigTransformer],
-      //  buildConfig: [buildConfigTransformer],
+    //  devConfig: [devConfigTransformer],
+    //  buildConfig: [buildConfigTransformer],
     //};
 
     const PerconaReactEnvEnv = react.compose([
@@ -78,8 +77,8 @@ export class PerconaReactEnvMain {
       react.overrideDependencies({
         devDependencies: {
           // '@types/react': '17.0.3'
-        }
-      })
+        },
+      }),
     ]);
     envs.registerEnv(PerconaReactEnvEnv);
     return new PerconaReactEnvMain();
