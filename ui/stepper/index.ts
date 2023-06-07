be1679b0b253de20de8bc191039dfd43f26e8df8
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-06-07 15:00:59 +0400

    EVEREST-56-styles-of-buttons: fix tests

--- ui/stepper/index.ts
+++ ui/stepper/index.ts
@@ -1,1 +1,2 @@
 export { Stepper } from './stepper';
+export type { StepperProps } from './stepper';
--- ui/stepper/stepper.spec.tsx
+++ ui/stepper/stepper.spec.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { DbToggleCard } from '../db-toggle-card/db-toggle-card';
-import { DbType } from '../db-toggle-card/db-toggle-card.types';
+import {HorizontalStepperWithConnectors, StepperWithoutConnectors} from './stepper.composition';
+
 
 describe('Stepper', () => {
-    it('Should not show the connector line when noConnector prop is true', () => {
-        //TODO fix
-        render(<DbToggleCard value={DbType.Mongo} />);
-        expect(screen.getByText('MongoDB')).toBeInTheDocument();
-        expect(screen.getByTestId('mongodb-toggle-button')).toBeInTheDocument();
-    });
+  it('should not show the connector line when noConnector prop is true', () => {
+    const {container} = render(<StepperWithoutConnectors/>);
+    expect(container.getElementsByClassName('MuiStepConnector-root')[0]).toBeUndefined();
+  })
+  it('should show the connector line when noConnector prop is false', () => {
+    const {container} = render(<HorizontalStepperWithConnectors/>);
+    console.log(container.getElementsByClassName('MuiStepConnector-root')[0]);
+    expect(container.getElementsByClassName('MuiStepConnector-root')[0]).toBeTruthy();
+  })
 });
-
-// describe('Stepper', () => {
-//   it('should not show the connector line when noConnector prop is true', () => {
-//     render(
-//       <div>
-//         {/*<Stepper noConnector dataTestId="noConnector">*/}
-//         {/*  {steps.map((label, index) => {*/}
-//         {/*    const stepProps: { completed?: boolean } = {};*/}
-//         {/*    return (*/}
-//         {/*      <Step key={`${label}_${index}`} {...stepProps}>*/}
-//         {/*        <StepLabel />*/}
-//         {/*      </Step>*/}
-//         {/*    );*/}
-//         {/*  })}*/}
-//         {/*</Stepper>*/}
-//       </div>
-//     );
-//   });
-//   expect(screen.getByTestId('noConnector-stepper')).toBeInTheDocument();
-// });
