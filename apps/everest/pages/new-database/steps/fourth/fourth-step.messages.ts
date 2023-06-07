17246a57ca069722906c08b427d72cba98f7c43c
Fabio Silva <ffjs1993@gmail.com>
2023-06-07 13:12:04 +0100

    everest-101 add messages file

---
+++ apps/everest/pages/new-database/steps/fourth/fourth-step.messages.ts
@@ -0,0 +1,8 @@
+export const Messages = {
+  externalAccess: 'External Access',
+  caption: `
+    Exposing your database to the internet poses severe risks, including unauthorized access, data breaches,
+    theft of sensitive information, data manipulation, compliance violations, legal consequences, and reputational damage.
+    Secure your database with strong controls, encryption, and firewalls. Use secure remote access, regularly back up data, and conduct security audits.
+  `,
+};
--- apps/everest/pages/new-database/steps/fourth/fourth-step.tsx
+++ apps/everest/pages/new-database/steps/fourth/fourth-step.tsx
@@ -1,15 +1,12 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import { Messages } from "./fourth-step.messages";
 
 export const FourthStep = () => {
   return (
     <>
-      <Typography variant="h6">External Access</Typography>
-      <Typography variant="caption">
-        Exposing your database to the internet poses severe risks, including unauthorized access, data breaches,
-        theft of sensitive information, data manipulation, compliance violations, legal consequences, and reputational damage.
-        Secure your database with strong controls, encryption, and firewalls. Use secure remote access, regularly back up data, and conduct security audits.
-      </Typography>
+      <Typography variant="h6">{Messages.externalAccess}</Typography>
+      <Typography variant="caption">{Messages.caption}</Typography>
     </>
   );
 }
