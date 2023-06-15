0fd06c4a9500623fb118ba19fa1515825c579ef5
Fabio Silva <ffjs1993@gmail.com>
2023-06-16 00:59:33 +0100

    chore: add favicon entry

--- apps/everest/everest.react-app.ts
+++ apps/everest/everest.react-app.ts
@@ -3,6 +3,7 @@ import { ReactAppOptions } from '@teambit/react';
 export const EverestApp: ReactAppOptions = {
   name: 'everest',
   entry: [require.resolve('./everest.app-root')],
+  favicon: require.resolve('./favicon.ico'),
 };
 
 export default EverestApp;
