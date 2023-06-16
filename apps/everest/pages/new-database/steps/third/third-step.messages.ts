c4d73a18a01569697e899017a832c802bb33f2bf
filipmikes1 <filipmikes1@users.noreply.github.com>
2023-06-16 15:04:15 +0000

    chore: format files. --skip-ci

--- apps/everest/pages/new-database/steps/third/third-step.messages.ts
+++ apps/everest/pages/new-database/steps/third/third-step.messages.ts
@@ -18,11 +18,12 @@ export const Messages = {
   at: 'at',
   am: 'AM',
   pm: 'PM',
-  infoText: (value) => `Everest will create a backup of your database every ${value}`,
+  infoText: (value) =>
+    `Everest will create a backup of your database every ${value}`,
   getTimeText: {
     hours: 'hours, starting at minute',
     days: 'days, at',
     weeks: 'weeks on',
-    months: 'months, on day'
-  }
+    months: 'months, on day',
+  },
 };
