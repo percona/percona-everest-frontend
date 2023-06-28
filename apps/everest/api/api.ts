35483b3c4df09f2a9f58495f49a3d6f29acdb5c7
Fabio Silva <ffjs1993@gmail.com>
2023-06-28 12:39:14 +0100

    feat: handle errors with snackbar

--- apps/everest/api/api.ts
+++ apps/everest/api/api.ts
@@ -1,7 +1,29 @@
 import axios from 'axios';
+import { enqueueSnackbar } from 'notistack';
 
 const BASE_URL = '/v1/';
+const DEFAULT_ERROR_MESSAGE = 'Something went wrong';
+const MAX_ERROR_MESSAGE_LENGTH = 120;
 
 export const api = axios.create({
   baseURL: BASE_URL
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status >= 400 && error.response.status <= 500) {
+      let message = DEFAULT_ERROR_MESSAGE;
+    
+      if (error.response.data && error.response.data.message) {
+        message = `${error.response.data.message.trim().substring(0, MAX_ERROR_MESSAGE_LENGTH)}...`;
+      }
+
+      enqueueSnackbar(message, {
+        variant: 'error'
+      });
+    }
+
+    return Promise.reject(error);
+  }
+ );
