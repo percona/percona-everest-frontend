78f6b8c9a570bceae2dfc2ca1e3d3585ce7267bc
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-07-03 13:04:17 +0400

    EVEREST-143-settings-page-routing: fix after merging main

--- apps/everest/components/drawer/Drawer.constants.ts
+++ apps/everest/components/drawer/Drawer.constants.ts
@@ -1,5 +1,7 @@
 import StorageIcon from '@mui/icons-material/Storage';
+import SettingsIcon from '@mui/icons-material/Settings';
 import { EverestRoute } from './Drawer.types';
+import {SettingsTabs} from "../../pages/settings/settings.types";
 
 export const DRAWER_WIDTH = 200;
 
@@ -7,4 +9,8 @@ export const ROUTES: EverestRoute[] = [{
   to: '/databases',
   icon: StorageIcon,
   text: 'Databases'
+}, {
+  to: `settings/${SettingsTabs.defaultConfigurations}`,
+  icon: SettingsIcon,
+  text: 'Settings',
 }];
