79271e095c8c11a645ebd85446e21ad38e733e59
Fabio Silva <ffjs1993@gmail.com>
2023-07-06 15:32:03 +0100

    fix: database fields

--- apps/everest/pages/new-database/steps/third/third-step.tsx
+++ apps/everest/pages/new-database/steps/third/third-step.tsx
@@ -17,8 +17,8 @@ import { DbWizardFormFields } from '../../new-database.types';
 
 export const ThirdStep = () => {
   const { control, watch } = useFormContext();
-  const backupsEnabled: boolean = watch('backupsEnabled');
-  const pitrEnabled: boolean = watch('pitrEnabled');
+  const backupsEnabled: boolean = watch(DbWizardFormFields.backupsEnabled);
+  const pitrEnabled: boolean = watch(DbWizardFormFields.pitrEnabled);
   const fetchedStorageValues = Object.values(StorageLocation);
 
   return (
