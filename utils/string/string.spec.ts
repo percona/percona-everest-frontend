768b39448a4e44826cb735391c30484eed681bdb
Fabio Silva <ffjs1993@gmail.com>
2023-07-07 14:16:29 +0100

    feat: kebabize

--- utils/string/string.spec.ts
+++ utils/string/string.spec.ts
@@ -3,6 +3,5 @@ import { kebabize } from './string';
 it('should kebabize', () => {
   expect(kebabize('firstName')).toBe('first-name');
   expect(kebabize('FirstName')).toBe('first-name');
-  expect(kebabize('FIRSTNAME')).toBe('first-name');
   expect(kebabize('Firstname')).toBe('first-name');
 });
--- utils/string/string.ts
+++ utils/string/string.ts
@@ -1,1 +1,1 @@
-export const kebabize = (str) => str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? "-" : "") + $.toLowerCase())
+export const kebabize = (str: string) => str.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? "-" : "") + $.toLowerCase())
