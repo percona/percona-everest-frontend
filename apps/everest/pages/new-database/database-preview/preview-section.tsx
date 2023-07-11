717cc43613164af4f7861e1492a385feb5fe0693
Fabio Silva <ffjs1993@gmail.com>
2023-07-11 21:22:12 +0100

    everest-138 add edit icon to preview-section

--- apps/everest/pages/new-database/database-preview/preview-section.tsx
+++ apps/everest/pages/new-database/database-preview/preview-section.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Stack, Typography } from "@mui/material";
+import { IconButton, Stack, Typography } from "@mui/material";
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import { PreviewContentTextProps, PreviewSectionProps } from './database-preview.types';
 
 export const PreviewSection = ({
@@ -10,39 +11,62 @@ export const PreviewSection = ({
   active = false,
   sx,
   ...stackProps
-}: PreviewSectionProps) => (
-  <Stack
-    sx={{
-      pl: 3,
-      pt: 1,
-      pb: 1,
-      pr: 1,
-      ...(!hasBeenReached && !active && {
-        pt: 0,
-        pb: 0,
-      }),
-      ...(active && {
-        backgroundColor: 'action.hover',
-      }),
-      ...sx,
-    }}
-    {...stackProps}
-  >
-    <Typography
-      variant={hasBeenReached ? 'sectionHeading' : 'caption'}
-      color={hasBeenReached ? 'text.primary' : 'text.disabled'}
+}: PreviewSectionProps) => {
+  const showEdit = !active && hasBeenReached;
+
+  return (
+    <Stack
       sx={{
-        '&::before': {
-          content: `"${order}. "`,
-          ml: -2,
-        }
+        pl: 3,
+        pt: 1,
+        pb: 1,
+        pr: 1,
+        ...(!hasBeenReached && !active && {
+          pt: 0,
+          pb: 0,
+        }),
+        ...(active && {
+          backgroundColor: 'action.hover',
+        }),
+        ...sx,
       }}
+      {...stackProps}
     >
-      {title}
-    </Typography>
-    {hasBeenReached && children}
-  </Stack>
-);
+      <Typography
+        // @ts-ignore 
+        variant={hasBeenReached ? 'sectionHeading' : 'caption'}
+        color={hasBeenReached ? 'text.primary' : 'text.disabled'}
+        sx={{
+          position: 'relative',
+          '&::before': {
+            content: `"${order}. "`,
+            ml: -2,
+          },
+        }}
+      >
+        {title}
+        {showEdit && (
+          <IconButton
+            // Absolute position to avoid the button's padding from interfering with the spacing
+            sx={{
+              position: 'absolute',
+              top: (theme) => theme.spacing(-1)
+            }}
+          >
+            <EditOutlinedIcon
+              fontSize='small'
+              color='primary'
+              sx={{
+                verticalAlign: 'text-bottom',
+              }}
+            />
+          </IconButton>
+        )}
+      </Typography>
+      {hasBeenReached && children}
+    </Stack>
+  );
+};
 
 export const PreviewContentText = ({ text }: PreviewContentTextProps) => (
   <Typography variant='caption' color='text.secondary'>{text}</Typography>
