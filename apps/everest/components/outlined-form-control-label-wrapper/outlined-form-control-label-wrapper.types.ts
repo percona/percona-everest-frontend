7b30b3c93f47308587524257079a2652af4b1197
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-07-12 15:51:05 +0400

    EVEREST-142-default-configurations: delete outlined-form-control-label-wrapper.tsx

--- apps/everest/components/outlined-form-control-label-wrapper/outlined-form-control-label-wrapper.types.ts
+++
@@ -1,7 +0,0 @@
-import { SxProps, Theme } from '@mui/material';
-import { FormControlLabelProps } from '../form-control-label/form-control-label.types';
-
-export interface FormControlLabelWrapperProps extends FormControlLabelProps {
-  children: React.ReactNode;
-  boxSx?: SxProps<Theme>;
-}
