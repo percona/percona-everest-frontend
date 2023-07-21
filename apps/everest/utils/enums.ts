b959ee1e53fdcfa35577bd37d4d9b42cd2e32fd3
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-07-21 22:59:38 +0400

    EVEREST-159-CRON-REDISIGN: remove extra function

--- apps/everest/utils/enums.ts
+++
@@ -1,6 +0,0 @@
-export const getEnumKeyByEnumValue = (enumObject, enumValue) => {
-  const keys = Object.keys(enumObject).filter(
-    (x) => enumObject[x] === enumValue
-  );
-  return keys.length > 0 ? keys[0] : null;
-};
