bc1e3da5b9724135ac5bda1f82923adbde1229d9
Fabio Silva <ffjs1993@gmail.com>
2023-08-01 00:13:45 +0100

    feat: use DialogTitle to show modal and redirect

--- apps/everest/pages/new-database/new-database.messages.ts
+++ apps/everest/pages/new-database/new-database.messages.ts
@@ -2,6 +2,13 @@ export const Messages = {
   previous: 'Previous',
   continue: 'Continue',
   createDatabase: 'Create database',
+  cancel: 'Cancel',
+  dialog: {
+    title: 'Are you sure you want to cancel?',
+    content: 'Cancelling will discard all your current changes to this database.',
+    reject: 'No',
+    accept: 'Yes, cancel',
+  },
   errors: {
     sourceRange: {
       invalid: 'Invalid IP address range',
