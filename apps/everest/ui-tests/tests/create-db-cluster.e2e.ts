0df6cb7b07e618af8f49d2569918a000b7294a3e
Fábio Silva <ffjs1993@gmail.com>
2023-08-03 10:33:55 +0100

    test: check installed versions (#43)
    * test: check installed versions
    
    * fix: simplify e2e test
    
    * fix: e2e test

--- apps/everest/ui-tests/tests/create-db-cluster.e2e.ts
+++ apps/everest/ui-tests/tests/create-db-cluster.e2e.ts
@@ -16,10 +16,26 @@ import { test, expect } from '@playwright/test';
 import { GetDbClusterPayload } from '../../types/dbCluster.types';
 
 let kubernetesId;
+const engineVersions = {
+  pxc: [],
+  psmdb: [],
+  postgresql: [],
+};
 
 test.beforeAll(async ({ request }) => {
   const kubernetesList = await request.get('/v1/kubernetes');
   kubernetesId = (await kubernetesList.json())[0].id;
+
+  const enginesList = await request.get(`/v1/kubernetes/${kubernetesId}/database-engines`);
+  const engines = (await enginesList.json()).items;
+
+  engines.forEach((engine) => {
+    const { type } = engine.spec;
+  
+    if (engine.status.status === 'installed') {
+      engineVersions[type].push(...Object.keys(engine.status.availableVersions.engine)); 
+    }
+  });
 });
 
 test.beforeEach(async ({ page }) => {
@@ -31,7 +47,25 @@ test.beforeEach(async ({ page }) => {
 test('Cluster creation', async ({ page, request }) => {
   const clusterName = 'db-cluster-ui-test';
 
-  await page.getByTestId('mongodb-toggle-button').click();
+  const dbEnginesButtons = page.getByTestId('toggle-button-group-input-db-type').getByRole('button');
+  const nrButtons = await dbEnginesButtons.count();
+
+  expect(nrButtons).toBe(3);
+
+  const mySqlButton = dbEnginesButtons.filter({ hasText: 'MySQL' });
+  const mongoButton = dbEnginesButtons.filter({ hasText: 'MongoDB' });
+
+  await expect(mySqlButton).toBeVisible();
+  await expect(mongoButton).toBeVisible();
+
+  await mongoButton.click();
+  await page.getByTestId('select-db-version-button').click();
+
+  const options = page.getByRole('option');
+
+  engineVersions.psmdb.forEach((version) => expect(options.filter({ hasText: new RegExp(`^${version}$`) })).toBeVisible());
+
+  await page.getByRole('option').first().click();
   await page.getByTestId('text-input-db-name').fill(clusterName);
   await page.getByTestId('db-wizard-continue-button').click();
 
