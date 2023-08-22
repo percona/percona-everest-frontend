46f461a9d41533bf3f52377bbae15ff42bcbf2d0
Fabio Silva <ffjs1993@gmail.com>
2023-08-22 10:40:39 +0100

    chore: remove unused imports

--- ui-lib/form/inputs/switch/switch.types.ts
+++ ui-lib/form/inputs/switch/switch.types.ts
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { FormControlLabelProps as MuiFormControlLabelProps, SxProps, Theme } from "@mui/material";
+import { FormControlLabelProps as MuiFormControlLabelProps } from "@mui/material";
 import { Control, UseControllerProps } from "react-hook-form";
 
 type FormControlLabelProps = MuiFormControlLabelProps;
