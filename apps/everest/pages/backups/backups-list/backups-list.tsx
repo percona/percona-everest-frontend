a562ca085e3303a844e04af8e80557b4b3377172
Filip Mikes <filip.mikes@3pillarglobal.com>
2023-08-23 14:19:38 +0200

    EVEREST-242 Created on demand backup modal

--- apps/everest/pages/backups/backups-list/backups-list.tsx
+++ apps/everest/pages/backups/backups-list/backups-list.tsx
@@ -1,22 +1,26 @@
-import React, { useMemo } from 'react';
-import { useParams } from 'react-router-dom';
-import { Table } from '@percona/ui-lib.table';
 import { MenuItem } from '@mui/material';
-import { MRT_ColumnDef } from 'material-react-table';
 import { MenuButton } from '@percona/ui-lib.menu-button';
+import { Table } from '@percona/ui-lib.table';
 import { format } from 'date-fns';
+import { MRT_ColumnDef } from 'material-react-table';
+import React, { useMemo, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { StatusField } from '../../../components/status-field/status-field';
+import { DATE_FORMAT } from '../../../constants';
 import { useDbBackups } from '../../../hooks/api/backups/useBackups';
 import { Backup, BackupStatus } from '../../../types/backups.types';
-import { DATE_FORMAT } from '../../../constants';
-import { StatusField } from '../../../components/status-field/status-field';
+import { OnDemandBackupModal } from '../on-demand-backup-modal/on-demand-backup-modal';
 import { BACKUP_STATUS_TO_BASE_STATUS } from './backups-list.constants';
 import { Messages } from './backups-list.messages';
 
 export const BackupsList = () => {
   const { dbClusterName } = useParams();
-
-  const { data: backups = [] } = useDbBackups(dbClusterName!, { enabled: !!dbClusterName, refetchInterval: 10 * 1000 });
-
+  const [openCreateBackupModal, setOpenCreateBackupModal] = useState(false);
+  const { data: backups = [] } = useDbBackups(dbClusterName!, {
+    enabled: !!dbClusterName,
+    refetchInterval: 10 * 1000,
+  });
+  console.log(backups);
   const columns = useMemo<MRT_ColumnDef<Backup>[]>(
     () => [
       {
@@ -24,7 +28,14 @@ export const BackupsList = () => {
         header: 'Status',
         filterVariant: 'multi-select',
         filterSelectOptions: Object.values(BackupStatus),
-        Cell: ({ cell }) => <StatusField status={cell.getValue<BackupStatus>()} statusMap={BACKUP_STATUS_TO_BASE_STATUS}>{cell.getValue()}</StatusField>
+        Cell: ({ cell }) => (
+          <StatusField
+            status={cell.getValue<BackupStatus>()}
+            statusMap={BACKUP_STATUS_TO_BASE_STATUS}
+          >
+            {cell.getValue()}
+          </StatusField>
+        ),
       },
       {
         accessorKey: 'name',
@@ -35,38 +46,59 @@ export const BackupsList = () => {
         header: 'Started',
         enableColumnFilter: false,
         sortingFn: 'datetime',
-        Cell: ({ cell }) => cell.getValue<Date>() ? format(cell.getValue<Date>(), DATE_FORMAT) : '',
+        Cell: ({ cell }) =>
+          cell.getValue<Date>()
+            ? format(cell.getValue<Date>(), DATE_FORMAT)
+            : '',
       },
       {
         accessorKey: 'completed',
         header: 'Finished',
         enableColumnFilter: false,
         sortingFn: 'datetime',
-        Cell: ({ cell }) => cell.getValue<Date>() ? format(cell.getValue<Date>(), DATE_FORMAT) : '',
+        Cell: ({ cell }) =>
+          cell.getValue<Date>()
+            ? format(cell.getValue<Date>(), DATE_FORMAT)
+            : '',
       },
     ],
     []
   );
 
   const handleManualBackup = (handleClose: () => void) => {
+    setOpenCreateBackupModal(true);
     handleClose();
-  }
+  };
+
+  const handleCloseBackupModal = () => {
+    setOpenCreateBackupModal(false);
+  };
 
   return (
-    <Table
-      noDataMessage={Messages.noData}
-      data={backups}
-      columns={columns}
-      renderTopToolbarCustomActions={() => (
-        <MenuButton buttonText={Messages.createBackup}>
-          {/* MUI Menu does not like fragments and asks for arrays instead */}
-          {(handleClose) => [
-              <MenuItem key="now" onClick={() => handleManualBackup(handleClose)}>{Messages.now}</MenuItem>,
-              <MenuItem key="schedule">{Messages.schedule}</MenuItem>
-            ]
-          }
-        </MenuButton>
-      )}
-    />
+    <>
+      <Table
+        noDataMessage={Messages.noData}
+        data={backups}
+        columns={columns}
+        renderTopToolbarCustomActions={() => (
+          <MenuButton buttonText={Messages.createBackup}>
+            {/* MUI Menu does not like fragments and asks for arrays instead */}
+            {(handleClose) => [
+              <MenuItem
+                key="now"
+                onClick={() => handleManualBackup(handleClose)}
+              >
+                {Messages.now}
+              </MenuItem>,
+              <MenuItem key="schedule">{Messages.schedule}</MenuItem>,
+            ]}
+          </MenuButton>
+        )}
+      />
+      <OnDemandBackupModal
+        open={openCreateBackupModal}
+        handleClose={handleCloseBackupModal}
+      />
+    </>
   );
 };
