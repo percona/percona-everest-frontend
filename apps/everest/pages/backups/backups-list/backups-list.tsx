6bf2ff3c27cd92601b5aae7afe04a179bb842f8a
Filip Mikes <filip.mikes@3pillarglobal.com>
2023-08-31 14:55:34 +0200

    EVEREST-246 created modals

--- apps/everest/pages/backups/backups-list/backups-list.tsx
+++ apps/everest/pages/backups/backups-list/backups-list.tsx
@@ -1,4 +1,6 @@
 import { Delete } from '@mui/icons-material';
+import AddIcon from '@mui/icons-material/Add';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import { MenuItem } from '@mui/material';
 import { MenuButton } from '@percona/ui-lib.menu-button';
 import { Table } from '@percona/ui-lib.table';
@@ -6,7 +8,7 @@ import { format } from 'date-fns';
 import { MRT_ColumnDef } from 'material-react-table';
 import React, { useMemo, useState } from 'react';
 import { useQueryClient } from 'react-query';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { DeleteDialog } from '../../../components/delete-dialog/delete-dialog';
 import { StatusField } from '../../../components/status-field/status-field';
 import { DATE_FORMAT } from '../../../constants';
@@ -21,7 +23,11 @@ import { BACKUP_STATUS_TO_BASE_STATUS } from './backups-list.constants';
 import { Messages } from './backups-list.messages';
 
 export const BackupsList = () => {
+  const navigate = useNavigate();
   const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+  const [openRestoreDialog, setOpenRestoreDialog] = useState(false);
+  const [openRestoreToNewDbDialog, setOpenRestoreToNewDbDialog] =
+    useState(false);
   const [selectedBackup, setSelectedBackup] = useState('');
   const queryClient = useQueryClient();
   const { dbClusterName } = useParams();
@@ -101,6 +107,34 @@ export const BackupsList = () => {
     });
   };
 
+  const handleRestoreBackup = (backupName: string) => {
+    setSelectedBackup(backupName);
+    setOpenRestoreDialog(true);
+  };
+
+  const handleCloseRestoreDialog = () => {
+    setOpenRestoreDialog(false);
+  };
+
+  const handleConfirmRestore = (backupName: string) => {
+    console.log('restoration');
+  };
+
+  const handleRestoreToNewDbBackup = (backupName: string) => {
+    setSelectedBackup(backupName);
+    setOpenRestoreToNewDbDialog(true);
+  };
+
+  const handleCloseRestoreToNewDbDialog = () => {
+    setOpenRestoreToNewDbDialog(false);
+  };
+
+  const handleConfirmRestoreToNewDb = (backupName: string) => {
+    navigate('/databases/new', {
+      state: { selectedDbCluster: dbClusterName!, backupName: backupName },
+    });
+  };
+
   return (
     <>
       <Table
@@ -125,7 +159,29 @@ export const BackupsList = () => {
         enableRowActions
         renderRowActionMenuItems={({ row, closeMenu }) => [
           <MenuItem
+            key={0}
+            onClick={() => {
+              handleRestoreBackup(row.original.name);
+              closeMenu();
+            }}
+            sx={{ m: 0, display: 'flex', gap: 1 }}
+          >
+            <RestartAltIcon />
+            {Messages.restore}
+          </MenuItem>,
+          <MenuItem
             key={1}
+            onClick={() => {
+              handleRestoreToNewDbBackup(row.original.name);
+              closeMenu();
+            }}
+            sx={{ m: 0, display: 'flex', gap: 1 }}
+          >
+            <AddIcon />
+            {Messages.restoreToNewDb}
+          </MenuItem>,
+          <MenuItem
+            key={2}
             onClick={() => {
               handleDeleteBackup(row.original.name);
               closeMenu();
@@ -150,6 +206,30 @@ export const BackupsList = () => {
           handleConfirm={handleConfirmDelete}
         >
           {Messages.deleteDialog.content}
+        </DeleteDialog>
+      )}
+      {openRestoreDialog && (
+        <DeleteDialog
+          isOpen={openRestoreDialog}
+          selectedId={selectedBackup}
+          closeModal={handleCloseRestoreDialog}
+          headerMessage={Messages.restoreDialog.header}
+          handleConfirm={handleConfirmRestore}
+          submitMessage={Messages.restoreDialog.submitButton}
+        >
+          {Messages.restoreDialog.content}
+        </DeleteDialog>
+      )}
+      {openRestoreToNewDbDialog && (
+        <DeleteDialog
+          isOpen={openRestoreToNewDbDialog}
+          selectedId={selectedBackup}
+          closeModal={handleCloseRestoreToNewDbDialog}
+          headerMessage={Messages.restoreDialogToNewDb.header}
+          handleConfirm={handleConfirmRestoreToNewDb}
+          submitMessage={Messages.restoreDialogToNewDb.submitButton}
+        >
+          {Messages.restoreDialogToNewDb.content}
         </DeleteDialog>
       )}
     </>
