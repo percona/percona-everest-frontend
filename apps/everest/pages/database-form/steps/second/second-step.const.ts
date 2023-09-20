5da3133337939b2143768e6b80acadc0bbf8629e
Fabio Silva <ffjs1993@gmail.com>
2023-09-21 00:26:35 +0100

    fix: resources as number, payload as string

--- apps/everest/pages/database-form/steps/second/second-step.const.ts
+++ apps/everest/pages/database-form/steps/second/second-step.const.ts
@@ -18,19 +18,19 @@ import { ResourceSize } from './second-step.types';
 
 export const DEFAULT_SIZES = {
   [ResourceSize.small]: {
-    [DbWizardFormFields.cpu]: "1",
-    [DbWizardFormFields.memory]: "2",
-    [DbWizardFormFields.disk]: "25",
+    [DbWizardFormFields.cpu]: 1,
+    [DbWizardFormFields.memory]: 2,
+    [DbWizardFormFields.disk]: 25,
   },
   [ResourceSize.medium]: {
-    [DbWizardFormFields.cpu]: "4",
-    [DbWizardFormFields.memory]: "8",
-    [DbWizardFormFields.disk]: "100",
+    [DbWizardFormFields.cpu]: 4,
+    [DbWizardFormFields.memory]: 8,
+    [DbWizardFormFields.disk]: 100,
   },
   [ResourceSize.large]: {
-    [DbWizardFormFields.cpu]: "8",
-    [DbWizardFormFields.memory]: "32",
-    [DbWizardFormFields.disk]: "200",
+    [DbWizardFormFields.cpu]: 8,
+    [DbWizardFormFields.memory]: 32,
+    [DbWizardFormFields.disk]: 200,
   },
 };
 
