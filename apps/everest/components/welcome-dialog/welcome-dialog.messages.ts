56669e7b9aeb7b7cbbc7cf883e94db91ecc7f2d0
Iaroslavna Soloveva <90199600+solovevayaroslavna@users.noreply.github.com>
2023-09-25 17:12:15 +0400

    EVEREST-455-welcome-message-fix: fix text (#107)
    (cherry picked from commit 0199f53951f7ba982cd5beb482ba83e774adfa0c)

--- apps/everest/components/welcome-dialog/welcome-dialog.messages.ts
+++ apps/everest/components/welcome-dialog/welcome-dialog.messages.ts
@@ -1,11 +1,9 @@
 export const Messages = {
   header: 'Welcome to Everest Alpha!',
-  subHead: `We're thrilled to welcome you to the Everest Alpha testing group! The
-Alpha version is intended for testing before the launch of the first
-public version in September. During this Alpha phase, we will be
-fine-tuning the functionality based on feedback we receive from you
-via this `,
-  notionPage: 'notion page',
+  subHead: `We're thrilled to welcome you to the Everest Alpha testing group! The Alpha version is intended for testing
+  before the upcoming Beta launch. During this Alpha phase, we will be fine-tuning the functionality based on feedback
+  we receive from you via `,
+  githubIssues: 'GitHub issues',
   subHead2: 'Ready to get started?',
   card1: {
     header: 'Create database',
