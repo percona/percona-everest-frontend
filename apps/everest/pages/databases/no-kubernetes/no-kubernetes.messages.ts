bad158291c050d1b6cb68c3191ab9f44c4cc6fd5
Iaroslavna Soloveva <90199600+solovevayaroslavna@users.noreply.github.com>
2023-09-26 22:05:57 +0400

    EVEREST-454-no-k8s-cluster-screen: fix text (#111)
    (cherry picked from commit 1bd32d380cd39dea94f7af57c5d2ef2da6893f39)

--- apps/everest/pages/databases/no-kubernetes/no-kubernetes.messages.ts
+++ apps/everest/pages/databases/no-kubernetes/no-kubernetes.messages.ts
@@ -15,8 +15,9 @@
 export const Messages = {
   noKubernetesClusters:
     "Oops! It looks like you don't have any Kubernetes clusters yet.",
-  letsCreate: 'Let’s create one:',
-  noKubernetesCommand: 'KUBECONFIG=~/.kube/config',
+  letsCreate: 'Let’s register one:',
+  exportKubeConfig: 'export KUBECONFIG = ~/.kube/config',
+  kubeconfig: '~/.kube/config',
   downloadTheLatest: 'Download the latest release of the ',
   everestctl: 'everestctl',
   toProvisionPercona: ' to provision Percona Everest.',
@@ -27,9 +28,9 @@ export const Messages = {
   modifyPermissions: 'Modify the file permissions:',
   modifyPermissionsCommand: 'chmod +x everestctl',
   fromTheInstallationWizard:
-    'From the installation wizard, provision and register the Kubernetes cluster in Everest using the following command.',
+    'From the installation wizard, provision and register the Kubernetes cluster in Everest using the following commands:',
   everestWillSearch: 'Everest will search for the kubeconfig file in the ',
-  ifYourFileIsLocated: ' path. If your file is located elsewhere, add the  ',
+  ifYourFileIsLocated: ' path. If your file is located elsewhere, set the ',
   beforeRunningTheCommand: ' environment variable before running the command:',
   everestctlInstall: './everestctl install operators',
   recheck: 'Recheck',
