9a803cc94ee399ad3c3ce1596020c30c3575bb33
Iaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-09-29 12:08:35 +0400

    EVEREST-407-db-engine-configuration: changing text

--- apps/everest/pages/database-form/steps/advanced-configurations/advanced-configurations.utils.ts
+++ apps/everest/pages/database-form/steps/advanced-configurations/advanced-configurations.utils.ts
@@ -5,24 +5,18 @@ export const getParamsPlaceholderFromDbType = (dbType: DbType) => {
 
   switch (dbType) {
     case DbType.Mongo:
-      dynamicText =
-        'operationProfiling:\n' + 'mode: slowOp\n' + 'slowOpThresholdMs: 200';
+      dynamicText = 'operationProfiling:\nmode: slowOp\nslowOpThresholdMs: 200';
       break;
     case DbType.Mysql:
       dynamicText =
-        '[mysqld]\n' +
-        'key_buffer_size=16M\n' +
-        'max_allowed_packet=128M\n' +
-        'max_connections=250';
+        '[mysqld]\nkey_buffer_size=16M\nmax_allowed_packet=128M\nmax_connections=250';
       break;
     case DbType.Postresql:
     default:
       dynamicText =
-        'log_connections = yes\n' +
-        'search_path = \'"$user", public\'\n' +
-        'shared_buffers = 128MB';
+        'log_connections = yes\nsearch_path = \'"$user", public\'\nshared_buffers = 128MB';
       break;
   }
 
-  return `Insert parameters:\n${dynamicText && `${dynamicText}`}`;
+  return dynamicText && `${dynamicText}`;
 };
