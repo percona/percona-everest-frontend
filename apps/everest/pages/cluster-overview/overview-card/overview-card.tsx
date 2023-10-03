da815b882bc30818bd5882043e9d00e064d0973f
Fabio Silva <ffjs1993@gmail.com>
2023-10-03 11:03:51 +0100

    chore: remove unused code

--- apps/everest/pages/cluster-overview/overview-card/overview-card.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { OverviewCardProps } from './overview-card.types';
-import { Box } from '@mui/material';
-
-export const OverviewCard = ({ title }: OverviewCardProps) => (
-  <Box>
-    
-  </Box>
-)
--- apps/everest/pages/cluster-overview/overview-card/overview-card.types.ts
+++
@@ -1,3 +0,0 @@
-export type OverviewCardProps = {
-  title: string
-}
