5d9f944f73f3bc272114fb85a80285806fb153d5
Fábio Silva <ffjs1993@gmail.com>
2023-10-11 16:03:26 +0100

    Everest-306: remove duplicate notifications (#120)
    * chore: prevent duplicate notifications
    
    * feat: disable add button

--- apps/everest/components/db-cluster-view/dbClusterView.types.ts
+++ apps/everest/components/db-cluster-view/dbClusterView.types.ts
@@ -14,12 +14,7 @@
 // limitations under the License.
 
 import { DbType } from '@percona/ui-lib.db-toggle-card';
-import { ReactNode } from 'react';
 import { DbEngineType } from '../../types/dbEngines.types';
-
-export interface DbClusterViewProps {
-  customHeader: ReactNode;
-}
 
 export interface DbTypeIconProviderProps {
   dbType: DbEngineType | DbType;
