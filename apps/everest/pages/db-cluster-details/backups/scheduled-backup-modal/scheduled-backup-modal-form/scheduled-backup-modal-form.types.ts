c35be186d59eb554fa8e6c4bb0d941bcd7b80200
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-10-25 12:21:13 +0400

    EVEREST-571-restore-of-a-scheduled-backup: fix schedule name

--- apps/everest/pages/db-cluster-details/backups/scheduled-backup-modal/scheduled-backup-modal-form/scheduled-backup-modal-form.types.ts
+++ apps/everest/pages/db-cluster-details/backups/scheduled-backup-modal/scheduled-backup-modal-form/scheduled-backup-modal-form.types.ts
@@ -19,7 +19,7 @@ import {
   TimeValue,
   WeekDays,
 } from '../../../../../components/time-selection/time-selection.types';
-import { MAX_RFC_1123_NAME_LENGTH } from '../../../../../constants';
+import { MAX_SCHEDULE_NAME_LENGTH } from '../../../../../constants';
 import { Messages } from '../scheduled-backup-modal.messages';
 
 export enum ScheduleFields {
@@ -41,10 +41,10 @@ export const schema = z.object({
   [ScheduleFields.name]: z
     .string()
     .nonempty()
-    .max(MAX_RFC_1123_NAME_LENGTH, Messages.scheduleName.tooLong)
+    .max(MAX_SCHEDULE_NAME_LENGTH, Messages.scheduleName.tooLong)
     .regex(
       doesNotContainerAnythingButAlphanumericAndDash,
-      `The schedule name should not exceed ${MAX_RFC_1123_NAME_LENGTH} characters.`
+      `The schedule name should not exceed ${MAX_SCHEDULE_NAME_LENGTH} characters.`
     )
     .regex(doesNotEndWithDash, "The name shouldn't end with a hyphen.")
     .regex(
