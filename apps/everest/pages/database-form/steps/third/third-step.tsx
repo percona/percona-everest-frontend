7ff9220334e93d5057fee2c8c3b923575811950d
Filip Mikes <filip.mikes@3pillarglobal.com>
2023-11-06 13:03:49 +0100

    EVEREST-580 Added new autofill component

--- apps/everest/pages/database-form/steps/third/third-step.tsx
+++ apps/everest/pages/database-form/steps/third/third-step.tsx
@@ -13,16 +13,16 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, { useEffect } from 'react';
 import { Box, Typography } from '@mui/material';
-import { AutoCompleteInput } from '@percona/ui-lib.form.inputs.auto-complete';
 import { SwitchInput } from '@percona/ui-lib.form.inputs.switch';
+import React from 'react';
 import { useFormContext } from 'react-hook-form';
+import { AutoCompleteAutoFill } from '../../../../components/auto-complete-auto-fill/auto-complete-auto-fill';
 import { TimeSelection } from '../../../../components/time-selection/time-selection';
 import { useBackupStorages } from '../../../../hooks/api/backup-storages/useBackupStorages';
 import { DbWizardFormFields } from '../../database-form.types';
-import { Messages } from './third-step.messages';
 import { useDatabasePageMode } from '../../useDatabasePageMode';
+import { Messages } from './third-step.messages';
 
 export const ThirdStep = () => {
   const { control, watch, setValue } = useFormContext();
@@ -30,14 +30,6 @@ export const ThirdStep = () => {
   const backupsEnabled: boolean = watch(DbWizardFormFields.backupsEnabled);
   // const pitrEnabled: boolean = watch(DbWizardFormFields.pitrEnabled);
   const { data: backupStorages = [], isFetching } = useBackupStorages();
-
-  useEffect(() => {
-    if (mode === 'new' && backupStorages?.length > 0) {
-      setValue(DbWizardFormFields.storageLocation, {
-        name: backupStorages[0].name,
-      });
-    }
-  }, [backupStorages]);
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
@@ -55,17 +47,12 @@ export const ThirdStep = () => {
             {Messages.repeatsEvery}
           </Typography>
           <TimeSelection showInfoAlert />
-          <AutoCompleteInput
+          <AutoCompleteAutoFill
             name={DbWizardFormFields.storageLocation}
             label={Messages.storageLocation}
             loading={isFetching}
             options={backupStorages}
-            autoCompleteProps={{
-              isOptionEqualToValue: (option, value) =>
-                option.name === value.name,
-              getOptionLabel: (option) =>
-                typeof option === 'string' ? option : option.name,
-            }}
+            enableFillFirst={mode === 'new'}
           />
           {/* <Typography variant="h6">{Messages.pitr}</Typography>
           <Typography variant="caption">{Messages.captionPitr}</Typography>
--- apps/everest/pages/db-cluster-details/backups/scheduled-backup-modal/scheduled-backup-modal-form/scheduled-backup-modal-form.tsx
+++ apps/everest/pages/db-cluster-details/backups/scheduled-backup-modal/scheduled-backup-modal-form/scheduled-backup-modal-form.tsx
@@ -13,18 +13,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React, { useContext, useEffect } from 'react';
 import { AutoCompleteInput } from '@percona/ui-lib.form.inputs.auto-complete';
 import { TextInput } from '@percona/ui-lib.form.inputs.text';
 import { LabeledContent } from '@percona/ui-lib.labeled-content';
-import { useParams } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import { AutoCompleteAutoFill } from '../../../../../components/auto-complete-auto-fill/auto-complete-auto-fill';
 import { TimeSelection } from '../../../../../components/time-selection/time-selection';
 import { useBackupStorages } from '../../../../../hooks/api/backup-storages/useBackupStorages';
 import { useDbCluster } from '../../../../../hooks/api/db-cluster/useDbCluster';
+import { ScheduleModalContext } from '../../backup.context';
 import { Messages } from '../scheduled-backup-modal.messages';
 import { ScheduleFields } from './scheduled-backup-modal-form.types';
-import { ScheduleModalContext } from '../../backup.context';
 
 export const ScheduledBackupModalForm = () => {
   const { watch } = useFormContext();
@@ -68,16 +69,12 @@ export const ScheduledBackupModalForm = () => {
         <TimeSelection showInfoAlert />
       </LabeledContent>
 
-      <AutoCompleteInput
+      <AutoCompleteAutoFill
         name={ScheduleFields.storageLocation}
         label={Messages.storageLocation}
         loading={isFetching}
         options={backupStorages}
-        autoCompleteProps={{
-          isOptionEqualToValue: (option, value) => option.name === value.name,
-          getOptionLabel: (option) =>
-            typeof option === 'string' ? option : option.name,
-        }}
+        enableFillFirst={mode === 'new'}
         isRequired
       />
     </>
