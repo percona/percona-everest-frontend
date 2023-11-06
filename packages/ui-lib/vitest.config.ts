1f3bb1a039a06db04cdbfe5a5785ce24d607ac7a
Fabio Silva <ffjs1993@gmail.com>
2023-11-06 15:58:41 +0000

    fix: CI

--- packages/ui-lib/vitest.config.ts
+++ packages/ui-lib/vitest.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitest/config'
+import * as path from 'path';
 
 export default defineConfig({
   test: {
@@ -6,4 +7,12 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: 'src/setupTests.ts',
   },
+  resolve: {
+    alias: {
+      "@percona/ui-lib": path.resolve(__dirname, '../../packages/ui-lib/src'),
+      "@percona/design": path.resolve(__dirname, '../../packages/design/src'),
+      "@percona/utils": path.resolve(__dirname, '../../packages/utils/src'),
+      "@percona/types": path.resolve(__dirname, '../../packages/types/src'),
+    }
+  }
 })
