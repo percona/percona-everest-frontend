0276863d3e6507959dac5c77f738bfa99d9a6a85
Fabio Silva <ffjs1993@gmail.com>
2023-11-14 14:46:27 +0000

    chore: add localization adapters

--- apps/everest/src/App.tsx
+++ apps/everest/src/App.tsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { ThemeContextProvider, everestThemeOptions } from '@percona/design';
 import { SnackbarProvider } from 'notistack';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import { DrawerContextProvider } from 'contexts/drawer/drawer.context';
 import { Main } from 'components/main/Main';
 
@@ -15,6 +17,7 @@ const App = () => {
   });
 
   return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
     <SnackbarProvider maxSnack={3} preventDuplicate>
       <QueryClientProvider client={queryClient}>
         <ThemeContextProvider themeOptions={everestThemeOptions}>
@@ -25,6 +28,7 @@ const App = () => {
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </SnackbarProvider>
+    </LocalizationProvider>
   );
 };
 
