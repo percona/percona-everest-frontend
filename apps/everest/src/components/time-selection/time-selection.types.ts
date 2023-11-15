e1c5b2958e978746e963840fbcdecd0332cc65f4
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2023-11-15 20:49:32 +0400

    EVEREST-485-schedules-in-dbWizard: fix build error

--- apps/everest/src/components/time-selection/time-selection.types.ts
+++ apps/everest/src/components/time-selection/time-selection.types.ts
@@ -13,6 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import z from 'zod';
 import { Theme, SxProps } from '@mui/material';
 import { timeSelectionSchema } from './time-selection.schema';
 
