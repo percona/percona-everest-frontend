d4243cc29f4ea7812cf1b9fb3914846b9ea3ebb7
Fabio Silva <ffjs1993@gmail.com>
2023-11-16 20:31:44 +0000

    feat: correct colors for Alert

---
+++ packages/design/src/themes/base/BaseTheme.utils.ts
@@ -0,0 +1,50 @@
+import { AlertColor, Theme } from '@mui/material';
+
+export const getFontColorForAlertSeverity = (
+  severity: AlertColor = 'success',
+  theme: Theme
+): string => {
+  switch (severity) {
+    case 'success':
+      return theme.palette.success.contrastText;
+    case 'info':
+      return theme.palette.info.contrastText;
+    case 'warning':
+      return theme.palette.warning.contrastText;
+    case 'error':
+      return theme.palette.error.contrastText;
+  }
+};
+
+export const getBorderColorForAlertSeverity = (
+  severity: AlertColor = 'success',
+  { palette: { mode } }: Theme
+): string => {
+  // These are PEAK's "outlineBorder" color. Since they are not used elsewhere, they live here
+  switch (severity) {
+    case 'success':
+      return mode === 'light' ? 'rgba(0, 116, 91, 0.20)' : '#00745B';
+    case 'info':
+      return mode === 'light' ? 'rgba(0, 86, 203, 0.20)' : '#0070E5';
+    case 'warning':
+      return mode === 'light' ? 'rgba(133, 110, 0, 0.20)' : '#F5CC00';
+    case 'error':
+      return mode === 'light' ? 'rgba(177, 8, 16, 0.20)' : '#CC352E';
+  }
+};
+
+export const getBackgroundColorForAlertSeverity = (
+  severity: AlertColor = 'success',
+  theme: Theme
+): string => {
+  switch (severity) {
+    case 'success':
+      return theme.palette.success.main;
+    case 'info':
+      return theme.palette.info.main;
+    case 'warning':
+      return theme.palette.warning.main;
+    case 'error':
+      return theme.palette.error.main;
+  }
+};
