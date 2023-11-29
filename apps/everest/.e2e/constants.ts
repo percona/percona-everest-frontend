413b0d86eda1f4efd9910a0cf33f6497a5e7c44c
Fabio Silva <ffjs1993@gmail.com>
2023-11-29 23:52:41 +0000

    chore: output files

--- apps/everest/.e2e/constants.ts
+++ apps/everest/.e2e/constants.ts
@@ -1,6 +1,1 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export const STORAGE_STATE_FILE = join(__dirname, '.auth/user.json');
+export const STORAGE_STATE_FILE = '.auth/user.json';
