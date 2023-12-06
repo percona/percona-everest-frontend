863c3de1b1b44222b372cb08571111df3a80ba75
Filip Mikes <filip.mikes@3pillarglobal.com>
2023-12-06 11:22:34 +0100

    EVEREST-621 Remove wrapper

--- apps/everest/src/components/restore-db-modal/RestoreDbModal.tsx
+++ apps/everest/src/components/restore-db-modal/RestoreDbModal.tsx
@@ -30,110 +30,106 @@ export const RestoreDbModal = () => {
     useDbClusterRestore(dbClusterName!);
 
   return (
-    <>
-      <FormDialog
-        size="XXXL"
-        isOpen={isOpenRestoreDbModal}
-        closeModal={closeRestoreDbModal}
-        headerMessage={
-          isNewClusterMode
-            ? Messages.headerMessageCreate
-            : Messages.headerMessage
+    <FormDialog
+      size="XXXL"
+      isOpen={isOpenRestoreDbModal}
+      closeModal={closeRestoreDbModal}
+      headerMessage={
+        isNewClusterMode ? Messages.headerMessageCreate : Messages.headerMessage
+      }
+      schema={schema}
+      submitting={restoringBackup}
+      defaultValues={defaultValues}
+      onSubmit={(data: FieldValues) => {
+        const backupNameStripped = data.backupList.split(' - ')[0];
+        if (isNewClusterMode) {
+          closeRestoreDbModal();
+          const selectedBackup = backups?.find(
+            (backup) => backup.name === backupNameStripped
+          );
+          navigate('/databases/new', {
+            state: {
+              selectedDbCluster: dbClusterName!,
+              backupName: backupNameStripped,
+              backupStorageName: selectedBackup,
+            },
+          });
+        } else {
+          restoreBackup(
+            { backupName: backupNameStripped },
+            {
+              onSuccess() {
+                closeRestoreDbModal();
+              },
+            }
+          );
         }
-        schema={schema}
-        submitting={restoringBackup}
-        defaultValues={defaultValues}
-        onSubmit={(data: FieldValues) => {
-          const backupNameStripped = data.backupList.split(' - ')[0];
-          if (isNewClusterMode) {
-            closeRestoreDbModal();
-            const selectedBackup = backups?.find(
-              (backup) => backup.name === backupNameStripped
-            );
-            navigate('/databases/new', {
-              state: {
-                selectedDbCluster: dbClusterName!,
-                backupName: backupNameStripped,
-                backupStorageName: selectedBackup,
-              },
-            });
-          } else {
-            restoreBackup(
-              { backupName: backupNameStripped },
-              {
-                onSuccess() {
-                  closeRestoreDbModal();
-                },
-              }
-            );
-          }
-        }}
-        submitMessage={isNewClusterMode ? Messages.create : Messages.restore}
-      >
-        <LoadableChildren loading={isLoading}>
-          <Typography variant="body1">
-            {isNewClusterMode ? Messages.subHeadCreate : Messages.subHead}
-          </Typography>
-          <RadioGroup
-            name={RestoreDbFields.backupType}
-            radioGroupFieldProps={{
-              sx: {
-                ml: 1,
+      }}
+      submitMessage={isNewClusterMode ? Messages.create : Messages.restore}
+    >
+      <LoadableChildren loading={isLoading}>
+        <Typography variant="body1">
+          {isNewClusterMode ? Messages.subHeadCreate : Messages.subHead}
+        </Typography>
+        <RadioGroup
+          name={RestoreDbFields.backupType}
+          radioGroupFieldProps={{
+            sx: {
+              ml: 1,
+              display: 'flex',
+              gap: 3,
+              '& label': {
                 display: 'flex',
-                gap: 3,
-                '& label': {
-                  display: 'flex',
-                  gap: '10px',
-                  alignItems: 'center',
-                  padding: 1,
-                  '& span': {
-                    padding: '0px !important',
-                  },
+                gap: '10px',
+                alignItems: 'center',
+                padding: 1,
+                '& span': {
+                  padding: '0px !important',
                 },
               },
-            }}
-            options={[
-              {
-                label: Messages.fromBackup,
-                value: BackuptypeValues.fromBackup,
-              },
-              {
-                label: Messages.fromPitr,
-                value: BackuptypeValues.fromPitr,
-                disabled: true,
-              },
-            ]}
-          />
-          <SelectInput
-            name={RestoreDbFields.backupList}
-            label={Messages.selectBackup}
-            selectFieldProps={{
-              displayEmpty: true,
-              renderValue: (value) => {
-                const stringValue = value as string;
-                if (value === '') {
-                  return <span>{Messages.emptyValue}</span>;
-                }
-                return <span>{stringValue}</span>;
-              },
-              sx: { minWidth: '80px' },
-            }}
-          >
-            {backups
-              ?.filter((value) => value.completed)
-              .map((value) => {
-                const valueWithTime = `${
-                  value.name
-                } - ${value.completed?.toLocaleString('en-US')}`;
-                return (
-                  <MenuItem key={value.name} value={valueWithTime}>
-                    {valueWithTime}
-                  </MenuItem>
-                );
-              })}
-          </SelectInput>
-        </LoadableChildren>
-      </FormDialog>
-    </>
+            },
+          }}
+          options={[
+            {
+              label: Messages.fromBackup,
+              value: BackuptypeValues.fromBackup,
+            },
+            {
+              label: Messages.fromPitr,
+              value: BackuptypeValues.fromPitr,
+              disabled: true,
+            },
+          ]}
+        />
+        <SelectInput
+          name={RestoreDbFields.backupList}
+          label={Messages.selectBackup}
+          selectFieldProps={{
+            displayEmpty: true,
+            renderValue: (value) => {
+              const stringValue = value as string;
+              if (value === '') {
+                return <span>{Messages.emptyValue}</span>;
+              }
+              return <span>{stringValue}</span>;
+            },
+            sx: { minWidth: '80px' },
+          }}
+        >
+          {backups
+            ?.filter((value) => value.completed)
+            .map((value) => {
+              const valueWithTime = `${
+                value.name
+              } - ${value.completed?.toLocaleString('en-US')}`;
+              return (
+                <MenuItem key={value.name} value={valueWithTime}>
+                  {valueWithTime}
+                </MenuItem>
+              );
+            })}
+        </SelectInput>
+      </LoadableChildren>
+    </FormDialog>
   );
 };
