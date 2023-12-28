58c79d2192417115f2e25c2d939110660a92d430
Fabio Silva <ffjs1993@gmail.com>
2023-12-28 15:54:11 +0000

    feat: add table for endpoints

--- apps/everest/src/pages/settings/monitoring-endpoints/monitoring-endpoints.tsx
+++ apps/everest/src/pages/settings/monitoring-endpoints/monitoring-endpoints.tsx
@@ -1,3 +1,31 @@
+import { Table } from '@percona/ui-lib';
+import { useMonitoringInstancesList } from 'hooks/api/monitoring/useMonitoringInstancesList';
+import { MRT_ColumnDef } from 'material-react-table';
+import { useMemo } from 'react';
+import { MonitoringInstance } from 'shared-types/monitoring.types';
+
 export const MonitoringEndpoints = () => {
-  return <div>Monitoring Endpoints</div>;
+  const { data: monitoringInstances = [] } = useMonitoringInstancesList();
+  const columns = useMemo<MRT_ColumnDef<MonitoringInstance>[]>(
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'Name',
+      },
+      {
+        accessorKey: 'url',
+        header: 'Endpoint',
+      },
+    ],
+    []
+  );
+
+  return (
+    <Table
+      hideExpandAllIcon
+      data={monitoringInstances}
+      columns={columns}
+      noDataMessage="No monitoring endpoint added"
+    />
+  );
 };
