f418ea0f43a5ed65efc4c9e50533931e8e85b71f
Filip Mikes <filip.mikes@3pillarglobal.com>
2024-01-08 14:03:13 +0100

    EVEREST-698 small fixes

--- apps/everest/src/hooks/api/backup-storages/__mocks__/useBackupStorages.ts
+++ apps/everest/src/hooks/api/backup-storages/__mocks__/useBackupStorages.ts
@@ -15,25 +15,19 @@
 
 import { StorageType } from 'shared-types/backupStorages.types';
 
-// was moved as separate object to avoid recreation since the original use of useQuery caches the data
-const backupStorageMockData = {
-  data: [
-    {
-      id: 'backup-storage-1',
-      name: 'Backup Storage One',
-      type: StorageType.S3,
-      bucketName: 'bucket-001',
-      region: 'Us',
-    },
-  ],
-};
-
-export const useBackupStorages = () => backupStorageMockData;
-
-export const useCreateBackupStorage = () => ({
+const storageDataObject = {
   id: 'backup-storage-1',
   name: 'Backup Storage One',
   type: StorageType.S3,
   bucketName: 'bucket-001',
   region: 'Us',
-});
+};
+
+// was moved as separate object to avoid recreation since the original use of useQuery caches the data
+const backupStorageMockData = {
+  data: [storageDataObject],
+};
+
+export const useBackupStorages = () => backupStorageMockData;
+
+export const useCreateBackupStorage = () => storageDataObject;
