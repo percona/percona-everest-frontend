3599cd6eadae9bab1755d5fecb850d09007d3b27
Filip Mikes <filip.mikes@3pillarglobal.com>
2024-01-15 10:42:34 +0100

    EVEREST-625 Added pitr for mongodb

--- apps/everest/src/api/dbClusterApi.ts
+++ apps/everest/src/api/dbClusterApi.ts
@@ -12,14 +12,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { api } from './api';
 import {
   DbCluster,
   GetDbClusterCredentialsPayload,
   GetDbClusterPayload,
 } from 'shared-types/dbCluster.types';
+import { api } from './api';
 
 export const createDbClusterFn = async (data: DbCluster) => {
+  console.log(data);
   const response = await api.post(`database-clusters`, data);
 
   return response.data;
