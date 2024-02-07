5080f5ddf48404cb99d50503829f15068a7a8d1c
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2024-02-07 20:08:04 +0400

    EVEREST-676-multinamespaces-for-db-wizard: creation, overview, list, setup tests

--- apps/everest/.e2e/db-cluster/db-cluster-overview.e2e.ts
+++ apps/everest/.e2e/db-cluster/db-cluster-overview.e2e.ts
@@ -1,15 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { createDbClusterFn, deleteDbClusterFn } from '../utils/db-cluster';
 import { getTokenFromLocalStorage } from '../utils/localStorage';
+import { getNamespacesFn } from '../utils/namespaces';
 
 test.describe('DB Cluster Overview', async () => {
   const dbClusterName = 'cluster-overview-test';
-  //TODO 676 getListOfNamespace, get the first one
-  const namespace = 'percona-everest';
+  let namespace = '';
 
   test.beforeAll(async ({ request }) => {
     const token = await getTokenFromLocalStorage();
-    await createDbClusterFn(token, request, namespace, {
+    const namespaces = await getNamespacesFn(token, request);
+    namespace = namespaces[0];
+    await createDbClusterFn(token, request, namespaces[0], {
       dbName: dbClusterName,
       dbType: 'mysql',
       numberOfNodes: '1',
