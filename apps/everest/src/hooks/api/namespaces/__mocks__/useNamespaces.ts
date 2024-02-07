082a8a09349a1463b6524dc27bd5fd6ec418a6f7
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2024-02-07 15:59:07 +0400

    EVEREST-676-multinamespaces-for-db-wizard: review by Nuna (fix of Namespace field order in dbWizard)

--- apps/everest/src/hooks/api/namespaces/__mocks__/useNamespaces.ts
+++ apps/everest/src/hooks/api/namespaces/__mocks__/useNamespaces.ts
@@ -14,6 +14,5 @@
 // limitations under the License.
 
 export const useNamespaces = () => {
-  console.log('useNameSpace api');
   return ['my-special-place', 'the-dark-side', 'percona-everest'];
 };
