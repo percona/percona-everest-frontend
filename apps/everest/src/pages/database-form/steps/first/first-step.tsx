de649dfe8cd0026c90f02f36cf00f0d92744437b
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2024-02-14 16:57:27 +0400

    EVEREST-835-dbtype-bug-two-namespaces-with-single-operator: code-review fix

--- apps/everest/src/pages/database-form/steps/first/first-step.tsx
+++ apps/everest/src/pages/database-form/steps/first/first-step.tsx
@@ -184,24 +184,17 @@ export const FirstStep = ({ loadingDefaultsForEdition }: StepProps) => {
       return;
     }
     const { isDirty: isNameDirty } = getFieldState(DbWizardFormFields.dbName);
+    const defaultDbType = dbEngineToDbType(dbEngines[0].type);
 
     if (!dbType) {
-      const defaultDbType = dbEngineToDbType(dbEngines[0].type);
       if (defaultDbType) {
-        setValue(
-          DbWizardFormFields.dbType,
-          dbEngineToDbType(dbEngines[0].type)
-        );
+        setValue(DbWizardFormFields.dbType, defaultDbType);
         setRandomDbName(defaultDbType);
       }
     } else {
       if (!dbEngines.find((engine) => engine.type === dbEngine)) {
-        const defaultDbType = dbEngineToDbType(dbEngines[0].type);
         if (defaultDbType) {
-          setValue(
-            DbWizardFormFields.dbType,
-            dbEngineToDbType(dbEngines[0].type)
-          );
+          setValue(DbWizardFormFields.dbType, defaultDbType);
           if (!isNameDirty) {
             setRandomDbName(defaultDbType);
           }
