618efa18c2c822b16d264e8a31c9237c3445347a
Fabio Silva <ffjs1993@gmail.com>
2024-02-15 11:27:06 +0000

    test: e2e

--- apps/everest/.e2e/db-cluster/db-wizard/create-db-cluster/create-db-cluster.e2e.ts
+++ apps/everest/.e2e/db-cluster/db-wizard/create-db-cluster/create-db-cluster.e2e.ts
@@ -24,6 +24,7 @@ import { pitrStepCheck } from './steps/pitr-step';
 import { resourcesStepCheck } from './steps/resources-step';
 import { getNamespacesFn } from '../../../utils/namespaces';
 import { deleteDbClusterFn } from '../../../utils/db-cluster';
+import { moveBack, moveForward } from '../../../utils/db-wizard';
 
 test.describe('DB Cluster creation', () => {
   let engineVersions = {
@@ -61,6 +62,35 @@ test.describe('DB Cluster creation', () => {
     // 1) rewrite the starting pipeline of launching everest to launch everest without clusters
     // 2) add 2 clusters using new methods from EVEREST-203
     // 3) check that the default parameters for MySQL are changed with parameters for the first available dbEngine
+  });
+
+  test('Cluster defaults', async ({ page }) => {
+    const expectedNodesOrder = [3, 3, 2];
+    const dbEnginesButtons = page
+      .getByTestId('toggle-button-group-input-db-type')
+      .getByRole('button');
+
+    expect(await dbEnginesButtons.count()).toBe(3);
+    // MySQL is our default DB type
+    expect(await page.getByTestId('mysql-toggle-button')).toHaveAttribute(
+      'aria-pressed',
+      'true'
+    );
+
+    for (let i = 0; i < 3; i++) {
+      await dbEnginesButtons.nth(i).click();
+      expect(
+        await page.getByTestId('select-input-db-version').inputValue()
+      ).toBeDefined();
+
+      await moveForward(page);
+
+      expect(
+        await page.getByTestId(`toggle-button-nodes-${expectedNodesOrder[i]}`)
+      ).toHaveAttribute('aria-pressed', 'true');
+
+      await moveBack(page);
+    }
   });
 
   test('Cluster creation', async ({ page, request }) => {
