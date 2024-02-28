6bff0cab66d1e3090c34341a92df6a9dd5eef444
solovevayaroslavna <iaroslavna.kozlova@simbirsoft.com>
2024-02-28 19:46:10 +0400

    EVEREST-839-multinamespace-e2e-tests: CI fix

--- apps/everest/.e2e/constants.ts
+++ apps/everest/.e2e/constants.ts
@@ -3,7 +3,7 @@ export const STORAGE_NAMES = ['test-storage-1', 'test-storage-2'];
 
 export enum EVEREST_CI_NAMESPACES {
   EVEREST_UI = 'everest-ui',
-  PSMDB_ONLY = 'everest-ui-psmdb',
-  PXC_ONLY = 'everest-ui-pxc',
-  PG_ONLY = 'everest-ui-pg',
+  PSMDB_ONLY = 'psmdb-only',
+  PXC_ONLY = 'pxc-only',
+  PG_ONLY = 'pg-only',
 }
