929b3ffc0bda1058f29e29288229c08828339d5c
Fabio Silva <ffjs1993@gmail.com>
2024-02-28 15:05:10 +0000

    chore: improve e2e test

--- apps/everest/.e2e/utils/db-wizard.ts
+++ apps/everest/.e2e/utils/db-wizard.ts
@@ -1,4 +1,5 @@
 import { expect, Page } from '@playwright/test';
+import { startTransition } from 'react';
 
 export const storageLocationAutocompleteEmptyValidationCheck = async (
   page: Page,
@@ -20,3 +21,37 @@ export const moveForward = (page: Page) =>
 
 export const moveBack = (page: Page) =>
   page.getByTestId('db-wizard-previous-button').click();
+
+export const goToStep = (
+  page: Page,
+  step:
+    | 'basic-information'
+    | 'resources'
+    | 'backups'
+    | 'point-in-time-recovery'
+    | 'advanced-configurations'
+    | 'monitoring'
+) => page.getByTestId(`button-edit-preview-${step}`).click();
+
+export const setPitrEnabledStatus = async (page: Page, checked: boolean) => {
+  const checkbox = page
+    .getByTestId('switch-input-pitr-enabled-label')
+    .getByRole('checkbox');
+
+  const isCheckboxChecked = await checkbox.isChecked();
+
+  if (checked !== isCheckboxChecked) {
+    await checkbox.click();
+  }
+
+  expect(await checkbox.isChecked()).toBe(checked);
+};
+
+export const submitWizard = (page: Page) => {
+  page.getByTestId('db-wizard-submit-button').click();
+};
+
+export const goToLastAndSubmit = async (page: Page) => {
+  await goToStep(page, 'monitoring');
+  await submitWizard(page);
+};
